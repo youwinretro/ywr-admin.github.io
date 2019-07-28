6d67f4b7c8186f15fbb7b55e0b235a364d100225
youwinretro <53099334+youwinretro@users.noreply.github.com>
2019-07-28 01:40:00 -0300

    Add files via upload

---
+++ precache-manifest.b3e26de9db922cf7baf0754733bc268e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a81770a2cb4e4a0f43ef",
+    "url": "/js/chunk-vendors.28175826.js"
+  },
+  {
+    "revision": "e2cd6c043be9627b2243",
+    "url": "/js/app.6d28aaf2.js"
+  },
+  {
+    "revision": "6b247844838eefcc9c0a8b2313e2e16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eca1e9067772523d129309f819702a4f",
+    "url": "/img/photo.eca1e906.jpg"
+  },
+  {
+    "revision": "044d51ffa29d97c5db8ab0a9da1385fa",
+    "url": "/img/fundo.044d51ff.jpg"
+  },
+  {
+    "revision": "a81770a2cb4e4a0f43ef",
+    "url": "/css/chunk-vendors.6c2004e9.css"
+  },
+  {
+    "revision": "e2cd6c043be9627b2243",
+    "url": "/css/app.afc94a3d.css"
+  }
+];
